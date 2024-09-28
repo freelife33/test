265cbece77169617a33bb560568cef6103b739e3
freelife33 <freelife_33@hotmail.com>
2024-09-28 22:26:14 +0300

    First commit

---
+++ app/src/main/java/com/example/javaders/ilk.java
@@ -0,0 +1,13 @@
+package com.example.javaders;
+
+public class ilk {
+    public static void main(String[] args) {
+
+
+        //Değişkenler
+        int x=2;
+        int y = 3;
+        int z= x+y;
+        
+    }
+}
